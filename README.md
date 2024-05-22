a4a962b1b3965c0d7d081fd1dce1c0f7aee7afbf
yuracoff18 <yurayt18@gmail.com>
2024-05-22 17:31:39 -0500

    Creacion de Menu

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# binaryToCharAsciiAndCharToBinary
+# De Binario a caracter y de Caracter a Binario
 Conversor de binario a carácter y de carácter a binario
+
+
+### Integrantes
+
+- Yura Hernandez  
+Usuario: yuracoff18
+
+- Martin Estrada  
+Usuario: 
+
---
+++ __main__.py
@@ -0,0 +1,30 @@
+"""
+Conversiones de binario a caracter y de caracter a binario
+
+Integrantes:
+    Yura Hernandez
+    Martin Estrada
+
+"""
+def main():
+    print("============[Conversiones]============")
+    print("""
+Seleccione una opcion
+
+1. De binarios a caracter
+2. De caracter a binario
+3. Salir
+""")
+    eleccion = int(input("Seleccion: "))
+    if eleccion == 1:
+        pass
+    elif eleccion == 2:
+        pass
+    elif eleccion == 3:
+        return 0
+    else:
+        print("Eleccion no valida\n")
+
+
+if __name__ == "__main__":
+    main()
